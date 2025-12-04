fd46dd3e08d4314f31ddf717fe01ea7141d94f5d
Marcos Yuri <marcos.yuriro@gmail.com>
2025-12-04 08:07:42 -0300

    Add files via upload

--- LibraryMy/src/Book.java
+++ LibraryMy/src/Book.java
@@ -1,3 +1,29 @@
 public class Book {
+    private String title;
+    private int anoLancamento;
+    private String gender;
+    private boolean lent;
+
+
+    //Getters e Setters:
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     
 }
